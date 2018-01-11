f69a2cb1c44af886f6833e99d828c0324bea89fa
Patrick Shinn <shinn16@marshall.edu>
2018-01-11 13:41:06 -0500

    menu options 1 and 2 started.

--- src/Main.java
+++ src/Main.java
@@ -3,7 +3,7 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args){
         // initial variables
-        boolean done = false;
+        boolean done = false, validKey = false;
         Crypto crypto = new Crypto();
         Scanner scanner = new Scanner(System.in);
         String filename, key;
@@ -12,11 +12,11 @@ public class Main {
         // begin menu
         System.out.println("Welcome to the Cryptomatic.");
         while(!done){
-            System.out.println("What would you like to do?\n" +
-                    "1. Enter the filename and key to encrypt\n" +
-                    "2. Enter the filename and key to decrypt\n" +
-                    "3. Enter the filename to brute force\n" +
-                    "4. Exit");
+            System.out.println("What would you like to do?.\n" +
+                    "1. Enter the filename and key to encrypt.\n" +
+                    "2. Enter the filename and key to decrypt.\n" +
+                    "3. Enter the filename to brute force.\n" +
+                    "4. Exit.");
             System.out.print(":> ");
 
             // begin user input
@@ -24,8 +24,33 @@ public class Main {
                 choice = Integer.parseInt(scanner.nextLine());
                 switch (choice){
                     case 1:
+                        System.out.println("Please enter a file location.");
+                        System.out.print(":> ");
+                        filename = scanner.nextLine();
+
+                        while(!validKey){
+                            System.out.println("Please enter a two character encryption key.");
+                            System.out.print(":> ");
+                            key = scanner.nextLine();
+
+                            if (key.length() == 2) validKey = true;
+                            else System.out.println("Please enter a valid key of length 2.\n");
+                        }
+
                         break;
                     case 2:
+                        System.out.println("Please enter a file location.");
+                        System.out.print(":> ");
+                        filename = scanner.nextLine();
+
+                        while(!validKey){
+                            System.out.println("Please enter a two character encryption key.");
+                            System.out.print(":> ");
+                            key = scanner.nextLine();
+
+                            if (key.length() == 2) validKey = true;
+                            else System.out.println("Please enter a valid key of length 2.\n");
+                        }
                         break;
                     case 3:
                         break;
@@ -39,6 +64,7 @@ public class Main {
             }catch (NumberFormatException e){
                 System.out.println("\"" + choice + "\"" + " was not a valid selection.");
             }
+            validKey = false;     // resets the validKey to false for next run
             System.out.println(); // blank line between runs for easier menu reading
         }
         System.out.println("Goodbye.");
@@ -52,15 +78,15 @@ class Crypto{
 
     }
 
-    void encrypt(){
+    void encrypt(String filename, String key){
 
     }
 
-    void decrypt(){
+    void decrypt(String filename, String key){
 
     }
 
-    void abbrakabra(){
+    void abbrakabra(String filename){
 
     }
 }
