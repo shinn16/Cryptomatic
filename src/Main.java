f9a458aad54018aa3bed4bd51004d2e1b28f7c26
Patrick Shinn <shinn16@marshall.edu>
2018-01-11 17:52:21 -0500

    Menu options optimized :)

--- src/Main.java
+++ src/Main.java
@@ -1,13 +1,25 @@
+import java.io.*;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.HashSet;
 import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args){
         // initial variables
-        boolean done = false, validKey = false;
-        Crypto crypto = new Crypto();
+        boolean done = false;
         Scanner scanner = new Scanner(System.in);
+        char[] data = null;
         String filename, key;
         int choice = 0;
+        Crypto crypto = null;
+
+        try{
+            crypto = new Crypto("Dictionary.txt");
+        }catch (FileNotFoundException e){
+            System.out.println("Fatal error, dictionary not found.");
+            System.exit(0);
+        }
 
         // begin menu
         System.out.println("Welcome to the Cryptomatic.");
@@ -24,33 +36,10 @@ public class Main {
                 choice = Integer.parseInt(scanner.nextLine());
                 switch (choice){
                     case 1:
-                        System.out.println("Please enter a file location.");
-                        System.out.print(":> ");
-                        filename = scanner.nextLine();
-
-                        while(!validKey){
-                            System.out.println("Please enter a two character encryption key.");
-                            System.out.print(":> ");
-                            key = scanner.nextLine();
-
-                            if (key.length() == 2) validKey = true;
-                            else System.out.println("Please enter a valid key of length 2.\n");
-                        }
-
+                        crypto.encrypt(getFileCharacters(), getUserKey());
                         break;
                     case 2:
-                        System.out.println("Please enter a file location.");
-                        System.out.print(":> ");
-                        filename = scanner.nextLine();
-
-                        while(!validKey){
-                            System.out.println("Please enter a two character encryption key.");
-                            System.out.print(":> ");
-                            key = scanner.nextLine();
-
-                            if (key.length() == 2) validKey = true;
-                            else System.out.println("Please enter a valid key of length 2.\n");
-                        }
+                        crypto.decrypt(getFileCharacters(), getUserKey());
                         break;
                     case 3:
                         break;
@@ -64,29 +53,94 @@ public class Main {
             }catch (NumberFormatException e){
                 System.out.println("\"" + choice + "\"" + " was not a valid selection.");
             }
-            validKey = false;     // resets the validKey to false for next run
             System.out.println(); // blank line between runs for easier menu reading
         }
         System.out.println("Goodbye.");
 
     }
+
+    private static char[] getFileCharacters(){
+        boolean validFile = true;
+        Scanner scanner = new Scanner(System.in);
+        String filename;
+        char[] data = null;
+
+        do{ // let the user input a file, if it is valid continue
+            try{
+                System.out.println("Please enter a file location.");
+                System.out.print(":> ");
+                filename = scanner.nextLine();
+                data = loadFile(filename);
+                validFile = true;
+            }catch (IOException e){
+                validFile = false;
+                System.out.println("Please enter a valid file location.\n");
+            }
+        }while (!validFile); // if the file is not valid, go until the file is valid.
+        return data;
+    }
+
+    private static char[] getUserKey(){
+        Scanner scanner = new Scanner(System.in);
+        boolean validKey = false;
+        String key = null;
+
+        while(!validKey){
+            System.out.println("Please enter a two character encryption key.");
+            System.out.print(":> ");
+            key = scanner.nextLine();
+
+            if (key.length() == 2) validKey = true;
+            else System.out.println("Please enter a valid key of length 2.\n");
+        }
+        return key.toCharArray();
+    }
+
+    private static char[] loadFile(String fileName) throws IOException{
+
+        System.out.print("Loading file...");
+
+        InputStream in = new FileInputStream(new File(fileName));
+        Reader reader = new InputStreamReader(in, "UTF-8");
+
+        // buffer for efficiency
+        Reader buffer = new BufferedReader(reader);
+
+        int x, pos = 0;
+        char[] characters = new char[0];
+        while ((x = buffer.read()) != -1) {
+            char ch = (char) x;
+            characters = Arrays.copyOf(characters, characters.length + 1);
+            characters[pos++] = ch;
+        }
+        System.out.println("Done!");
+        return characters;
+    }
 }
 
 class Crypto{
+    private HashSet<String> dictionary = new HashSet<>();
 
-    Crypto(){
-
+    Crypto(String dictionaryFile) throws FileNotFoundException{
+        Scanner scanner = new Scanner(new File(dictionaryFile));
+        while (scanner.hasNextLine()){
+            String line = scanner.nextLine(); // gets the line
+            String[] words = line.split(" "); // turns the line into words
+            Collections.addAll(dictionary, words); // adds the words to the dictionary
+        }
     }
 
-    void encrypt(String filename, String key){
-
+    String encrypt(char[] filename, char[] key){
+        String encrypted = "";
+        return encrypted;
     }
 
-    void decrypt(String filename, String key){
-
+    String decrypt(char[] filename, char[] key){
+        String decrypted = "";
+        return decrypted;
     }
 
-    void abbrakabra(String filename){
+    void bruteForce(String filename){
 
     }
 }
